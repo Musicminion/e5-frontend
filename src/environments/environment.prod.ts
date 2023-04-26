87f939c7854ea0d41870eaf460c3f66d72082668
ziqianzhang <zhangziqian@sjtu.edu.cn>
2023-04-26 12:10:27 +0800

    fix: API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 
-let proxyUrl = window.location.protocol;
-let hostname = window.location.hostname;
+// let proxyUrl = window.location.protocol;
+// let hostname = window.location.hostname;
 
 export const environment = {
     production: true,
-    // apiUrl: '/api'
-    apiUrl: proxyUrl + '//' + hostname + ':8080'
+    apiUrl: '/api'
+    // apiUrl: proxyUrl + '//' + hostname + ':8080'
 };
